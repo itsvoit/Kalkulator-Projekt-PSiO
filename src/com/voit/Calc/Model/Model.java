2448afc28e4929bb924c84ce5e5c0a0941e21183
itsvoit <wojciech.skrzypiec2@gmail.com>
2021-12-09 15:28:26 +0100

    Add every needed method to ModelInterface and Model - add getters - add input comma - add clear operations - add simple operations - add equals method - add complex operations

--- src/com/voit/Calc/Model/Model.java
+++ src/com/voit/Calc/Model/Model.java
@@ -12,8 +12,39 @@ public class Model implements ModelInterface, ModelObservable {
 
 	public Model(){
 		observers = new ArrayList<>();
+		history = new ArrayList<>();
+		operations = new boolean[N_OPERATIONS];
 	}
 
+	//Getters
+
+	public Number getX() {
+		return x;
+	}
+
+	public Number getY() {
+		return y;
+	}
+
+	public Number getMemory() {
+		return memory;
+	}
+
+	public int getOperation() {
+		int operation = NO_OP;
+		for (int i=0; i<operations.length; i++)
+			if (operations[i]) {
+				operation = i;
+				break;
+			}
+
+		return operation;
+	}
+
+	//Calculator operations
+
+	//todo check input-related functions
+	//Input operations
 	public void appendNumber(int value){
 		intValue *= 10;
 		intValue += value;
@@ -21,7 +52,19 @@ public class Model implements ModelInterface, ModelObservable {
 	}
 
 	public void deductNumber() {
+		if (!x.fractional && x.intLen == 0) return;
 
+		if (x.fractional && x.fractionLen == 0) {
+			x.fractional = false;
+		}
+		else if (x.fractional && x.fractionLen > 0){
+			x.fractionLen--;
+			x.fractionVal /= 10;
+		} else if (!x.fractional && x.intLen > 0) {
+			x.intLen--;
+			x.intVal /= 10;
+		}
+		notifyObservers();
 	}
 
 	//todo memory-related functions
@@ -34,14 +77,131 @@ public class Model implements ModelInterface, ModelObservable {
 	}
 
 	public void memoryAdd() {
+		if (!memory.fractional && memory.intLen > 17) return;
+		if (memory.fractional && memory.fractionLen > 17) return;
 
+		double value = x.getValue() + memory.getValue();
+		memory.setFields(value);
+		notifyObservers();
 	}
 
 	public void memorySubtract() {
+		if (!memory.fractional && memory.intLen > 17) return;
+		if (memory.fractional && memory.fractionLen > 17) return;
 
+		double value = memory.getValue() - x.getValue();
+		memory.setFields(value);
+		notifyObservers();
 	}
 
 	public void memoryWrite() {
+
+	}
+
+	//todo simple operation-related functions
+	//Simple operations
+	public void add() {
+		operations[ADD] = true;
+		y = x.clone();
+		x = new Number();
+		notifyObservers();
+	}
+
+	public void subtract() {
+		operations[SUBTRACT] = true;
+		y = x.clone();
+		x = new Number();
+		notifyObservers();
+	}
+
+	public void multiply() {
+		operations[MULTIPLY] = true;
+		y = x.clone();
+		x = new Number();
+		notifyObservers();
+	}
+
+	public void divide() {
+		operations[DIVIDE] = true;
+		y = x.clone();
+		x = new Number();
+		notifyObservers();
+	}
+
+	public void negate() {
+		x.negative = !x.negative;
+		notifyObservers();
+	}
+
+	//todo equals function
+	//Equals method
+	public void equals() {
+		int operation = -1;
+		operation += 1 + getOperation();
+		if (operation == -1) return;
+
+		double value = 0;
+
+		switch (operation){
+			case ADD:
+				value = x.getValue();
+				value += y.getValue();
+				x.setFields(value);
+				break;
+
+			case SUBTRACT:
+				value = x.getValue();
+				value -= y.getValue();
+				x.setFields(value);
+				break;
+
+			case MULTIPLY:
+				value = x.getValue();
+				value *= y.getValue();
+				x.setFields(value);
+				break;
+
+			case DIVIDE:
+				value = x.getValue();
+				if (y.getValue() == 0) return;
+
+				value /= y.getValue();
+				x.setFields(value);
+				break;
+
+			case POWER:
+				value = x.getValue();
+				value = Math.pow(value, y.getValue());
+				x.setFields(value);
+				break;
+		}
+		y = new Number();
+
+		notifyObservers();
+	}
+
+	//todo complex operation-related functions
+	//Complex operations
+	public void percent() {
+
+	}
+
+	public void reciprocal() {
+
+	}
+
+	public void power() {
+		operations[POWER] = true;
+		y = x.clone();
+		x = new Number();
+		notifyObservers();
+	}
+
+	public void sqrt() {
+
+	}
+
+	public void log() {
 
 	}
 
@@ -60,5 +220,14 @@ public class Model implements ModelInterface, ModelObservable {
 	public void notifyObservers() {
 		//todo notify observers method
 		System.out.println("Notify observers");
+		for (int i=0; i<observers.size(); i++){
+			observers.get(i).update();
+		}
+	}
+
+	//Helper methods
+
+	private void clearOperations(){
+		for (int i=0; i<operations.length; i++) operations[i] = false;
 	}
 }
--- src/com/voit/Calc/Model/ModelInterface.java
+++ src/com/voit/Calc/Model/ModelInterface.java
@@ -1,12 +1,36 @@
 package com.voit.Calc.Model;
 
 public interface ModelInterface {
+	Number getX();
+	Number getY();
+	Number getMemory();
+	int getOperation();
+
 	void appendNumber(int value);
 	void deductNumber();
+	void comma();
+
+	void clear();
+	void clearAll();
 
 	void memoryClear();
 	void memoryRead();
 	void memoryAdd();
 	void memorySubtract();
 	void memoryWrite();
+
+	void add();
+	void subtract();
+	void multiply();
+	void divide();
+	void negate();
+
+	void equals();
+
+	void percent();
+	void reciprocal();
+	void power();
+	void sqrt();
+	void log();
+
 }

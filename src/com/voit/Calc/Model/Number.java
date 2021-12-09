a8f84e8b8002fbcbfdf70a5588a14a2a1048745f
itsvoit <wojciech.skrzypiec2@gmail.com>
2021-12-09 20:11:05 +0100

    Fixes and debug of Number - fix setValue not setting value correctly - make getString return correct string    * no trailing '0'    * no comma if fraction is equal 0 - fix fractional values - fix not showing negative values - add debug messages

--- src/com/voit/Calc/Model/Number.java
+++ src/com/voit/Calc/Model/Number.java
@@ -25,35 +25,51 @@ public class Number implements Cloneable{
 		double value = 0;
 		value += intVal;
 
-		value += (Math.pow(Math.pow(1, fractionLen), -1) * fractionVal);
+		value += (Math.pow(Math.pow(10, fractionLen), -1) * fractionVal);
+
+		value *= negative ? -1 : 1; //if negative, then make it negative, duh...
 
 		return value;
 	}
 
 	public String getString(){
-		return Double.toString(getValue());
+		String text = "";
+		text += intVal;
+		if (fractional) {
+			text += ".";
+			if (fractionVal != 0)
+				text += fractionVal;
+		}
+		return text;
 	}
 
 	public void setValue(double value){
 		intVal = (int) value;
 		intLen = getLen(intVal);
+		System.out.println("==========================="); //todo comment
+		System.out.printf("SET VALUE\nval: %f\nintVal: %d\n", value, intVal);
 
 		negative = intVal < 0;
 
 		double fractionPart = value - intVal;
 		String fractionString = Double.toString(fractionPart);
+//		System.out.println(fractionString);
 		char[] fractionChars = new char[fractionString.length()];
 		fractionString.getChars(0, fractionString.length(), fractionChars, 0);
 
 		long fractionLong = 0;
 		int len=0;
-		for (; len<18 && len<fractionChars.length; len++){
+		for (; len<18 && len+2<fractionChars.length; len++){
 			fractionLong *= 10;
-			fractionLong += Character.getNumericValue(fractionChars[len]);
+			fractionLong += Character.getNumericValue(fractionChars[len+2]);
 		}
 
 		fractionVal = fractionLong;
 		fractionLen = len;
+		System.out.println("fraction len: " + len); //todo comment
+
+		System.out.printf("fraction: %f\nf long: %d\ngetVal: %f\n", fractionPart, fractionLong, getValue());
+		System.out.println("===========================");
 
 		fractional = fractionVal != 0; //fractionVal != 0 ==> fractional = true
 	}

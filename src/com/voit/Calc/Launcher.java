b3202cd14f3f00ae6692b0e57916bc063d8410bc
itsvoit <wojciech.skrzypiec2@gmail.com>
2021-12-16 21:30:06 +0100

    Change the Launcher to use new ModelOnDouble

--- src/com/voit/Calc/Launcher.java
+++ src/com/voit/Calc/Launcher.java
@@ -2,8 +2,7 @@ package com.voit.Calc;
 
 import com.voit.Calc.Controller.Controller;
 import com.voit.Calc.Controller.ControllerInterface;
-import com.voit.Calc.Model.Model;
-import com.voit.Calc.Model.ModelInterface;
+import com.voit.Calc.Model.ModelOnDouble;
 
 public class Launcher {
 	public static void main(String[] args){
@@ -12,9 +11,9 @@ public class Launcher {
 	}
 
 	private void go(){
-		ModelInterface model = new Model();
-		ControllerInterface controller1 = new Controller(model); //controller using model
+//		ModelInterface model = new Model();
+//		ControllerInterface controller1 = new Controller(model); //controller using model
 
-//		ControllerInterface controller2 = new Controller(new ModelOnDouble()); //controller using model on double
+		ControllerInterface controller2 = new Controller(new ModelOnDouble()); //controller using model on double
 	}
 }

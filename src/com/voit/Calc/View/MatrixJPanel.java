e900dbc79a01f36720a6805233f95711374400a4
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-01-04 17:43:42 +0100

    Add update() to MatrixJPanel

--- src/com/voit/Calc/View/MatrixJPanel.java
+++ src/com/voit/Calc/View/MatrixJPanel.java
@@ -1,19 +1,24 @@
 package com.voit.Calc.View;
 
 import com.voit.Calc.Model.ModelInterface;
+import com.voit.Calc.Model.ModelObservers.ModelObserver;
+import com.voit.Calc.Model.ModelObservers.ModelUpdateEvent;
 
 import javax.swing.*;
 
-public class MatrixJPanel extends JPanel {
-
-	ModelInterface model;
+public class MatrixJPanel extends JPanel implements ModelObserver {
 
 	public MatrixJPanel(ModelInterface model){
-		this.model = model;
+
 	}
 
 	private void loadMatrices(){
 		//todo load matrices
+
+	}
+
+	@Override
+	public void update(ModelUpdateEvent e) {
 
 	}
 }
